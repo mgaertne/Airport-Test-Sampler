fdd86d27048b62ab62c72ed55dfc72825bdfd868
Markus Gärtner <markus.gaertner@it-agile.de>
2017-01-11 00:03:22 +0100

    upgraded to latest FitNesse version and latest Selenium version

--- FitNesse/src/org/airport/parkcalc/helper/BrowserDriver.java
+++ FitNesse/src/org/airport/parkcalc/helper/BrowserDriver.java
@@ -1,13 +1,17 @@
 package org.airport.parkcalc.helper;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public class BrowserDriver {
 
 	public static WebDriver driver;
 
 	public static void startBrowserAt(String url) {
-		driver = new FirefoxDriver();
+		ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
 		driver.navigate().to(url);
 	}
 
