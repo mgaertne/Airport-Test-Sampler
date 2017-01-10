a093698fa8cc7be3ded4606589ab8d6eb364b436
Markus Gärtner <markus.gaertner@it-agile.de>
2017-01-10 23:31:22 +0100

    upgraded CucumberJVM and Selenium to latest versions

--- CucumberJVM/src/test/java/org/airport/parkcalc/cucumber/ParkCalcTests.java
+++ CucumberJVM/src/test/java/org/airport/parkcalc/cucumber/ParkCalcTests.java
@@ -5,13 +5,16 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = { "junit:target/cucumber-junit-report.xml", "pretty" }, features = "features")
+@CucumberOptions(plugin = { "junit:target/cucumber-junit-report.xml", "pretty" }, features = "features")
 public class ParkCalcTests {
 
 	private static WebDriver driver;
@@ -19,7 +22,8 @@ public class ParkCalcTests {
 
 	@BeforeClass
 	public static void SuiteSetUp() {
-		driver = new FirefoxDriver();
+		ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
 		driver.navigate().to("http://www.shino.de/parkcalc");
 		page = PageFactory.initElements(driver, ParkCalcPage.class);
 	}
